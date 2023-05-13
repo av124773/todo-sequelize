f75f1189d853ff0ba0e0c06966de491b8623748f
av124773 <av124773@gmail.com>
2023-05-13 23:22:03 +0800

    feat: add facebook & dotenv

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
-const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const routes = require('./routes')
 const usePassport = require('./config/passport')
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
