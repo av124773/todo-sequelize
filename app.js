86993652e009ce2388998e5345232baab4d89604
Kevin_L <54697436+av124773@users.noreply.github.com>
2023-07-12 10:44:51 +0800

    Update app.js
    此修改是為了佈署至cloud run

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV !== 'production') {
 const routes = require('./routes')
 const usePassport = require('./config/passport')
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 8080 // 3001
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
